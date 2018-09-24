ff6041f6467b040555af240c8c778539ff723d3d
jmarcelfpg <jmarcel_fpg@hotmail.com>
2018-09-23 19:48:46 -0700

    All Requests working

--- app/src/main/java/com/example/admin/test2/QRScan.java
+++ app/src/main/java/com/example/admin/test2/QRScan.java
@@ -2,7 +2,10 @@ package com.example.admin.test2;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.pm.PackageManager;
@@ -12,13 +15,21 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
-import java.io.IOException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
 
 public class QRScan extends AppCompatActivity {
 
@@ -26,15 +37,15 @@ public class QRScan extends AppCompatActivity {
     CameraSource cameraSource;
     TextView textView;
     BarcodeDetector barcodeDetector;
-
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscan);
 
-        surfaceView= (SurfaceView)findViewById(R.id.cameraPreview);
-        textView= (TextView)findViewById(R.id.textView);
+        surfaceView= findViewById(R.id.cameraPreview);
+        textView= findViewById(R.id.textView);
 
         barcodeDetector=new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.QR_CODE).build();
 
@@ -72,6 +83,7 @@ public class QRScan extends AppCompatActivity {
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
                 final SparseArray<Barcode> qrcodes=detections.getDetectedItems();
+                final RequestQueue queue = Volley.newRequestQueue(QRScan.this);
                 if(qrcodes.size()!=0 ){
                     textView.post(new Runnable() {
                         @Override
@@ -79,6 +91,45 @@ public class QRScan extends AppCompatActivity {
                             Vibrator vibrator=(Vibrator)getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
                             vibrator.vibrate(1000);
                             textView.setText(qrcodes.valueAt(0).displayValue);
+
+                            String url = "https://laboratorioasesores.com/NewSIIL/Mantenimiento/Development/getEquipo.php";
+                            JSONObject jsonObject = new JSONObject();
+                            try {
+                                jsonObject.put("id", qrcodes.valueAt(0).displayValue);
+                            }catch (JSONException e){
+                                e.printStackTrace();
+                            }
+
+                            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST ,url, jsonObject, new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Intent i = new Intent(getApplicationContext(),Results.class);
+                                    i.putExtra("equipo", response.toString());
+                                    startActivity(i);
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(QRScan.this);
+                                    builder.setTitle("Error");
+                                    builder.setMessage(error.getMessage());
+                                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.cancel();
+                                        }
+                                    });
+
+                                    builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.cancel();
+                                        }
+                                    });
+                                    builder.show();
+                                }
+                            });
+                            queue.add(jsonObjectRequest);
                         }
                     });
                 }
